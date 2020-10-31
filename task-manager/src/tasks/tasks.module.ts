6193a58cf693dc836354be68ee67fa9384d60ac1
Alain D'Ettorre <alain.det@gmail.com>
2020-11-01 00:41:19 +0100

    Add controller, service and module boilerplate for Tasks

--- task-manager/src/tasks/tasks.module.ts
+++ task-manager/src/tasks/tasks.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+
+@Module({
+  controllers: [
+    TasksController,
+  ],
+  providers: [
+    TasksService,
+  ],
+})
 export class TasksModule {}
---
+++ task-manager/src/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { Task, TaskStatus } from './tasks.model';
+
+@Injectable()
+export class TasksService {
+
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.Open,
+    };
+
+    this.tasks = [...this.tasks, task];
+
+    return task;
+  }
+}
