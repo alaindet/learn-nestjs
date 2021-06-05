d1706e50d8916bfdaea9ec21357ab87c0054daea
Alain D'Ettorre <alain.det@gmail.com>
2021-06-06 01:43:04 +0200

    Add Passport local strategy

--- events-app/backend/src/auth/auth.module.ts
+++ events-app/backend/src/auth/auth.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from './entities/user.entity';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
-    
+    TypeOrmModule.forFeature([
+      User,
+    ]),
   ],
   controllers: [
     
   ],
   providers: [
-    
+    LocalStrategy,
   ],
 })
 export class AuthModule {}
