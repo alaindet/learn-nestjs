c6bb20ae1d7dc987dffa39c0abcf3f7e2b09f870
Alain D'Ettorre <alain.det@gmail.com>
2021-07-02 18:48:05 +0200

    WIP: Update docker-compose.yaml of admin app

--- admin-app/backend/src/app.module.ts
+++ admin-app/backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
+// TODO: Move to environment
+const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'database',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'adminapp',
+
+  // TODO: Do not use these in production
+  autoLoadEntities: true,
+  synchronize: true,
+};
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
